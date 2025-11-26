2ff9e3f27d77bfef06473c3147d240d8985afaca
fabiomirasola <mirasolafabio@hotmail.com>
2025-11-26 14:24:12 +0100

    Ajout page register

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,7 +1,93 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3333/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ fullName, email, password }),
+      });
+
+      if (!res.ok) throw new Error("Invalid credentials");
+
+      const data = await res.json();
+
+      if(data){
+        alert("Registration successful");
+        return navigate("/login");
+      } 
+
+    } catch (err) {
+      alert("Register failed");
+    }
+  }
+
+
   return (
-    <div className="p-4 text-center text-2xl">
-      Page d'inscription
+    <div className="p-4 text-center bg-gray-100 h-screen flex flex-col items-center justify-center">
+      <div className="bg-white p-8  shadow-md inline-block rounded-2xl w-100 ">
+        <div>
+          <p className="text-3xl">💵</p>
+          <h1 className="text-3xl">Finance Tracker</h1>
+        </div>
+        <div>
+          <form className="flex flex-col gap-4 mt-4 max-w-sm mx-auto" 
+            onSubmit={confirmPassword===password? 
+              handleSubmit: (e) => {e.preventDefault(); alert("Passwords do not match")}
+            }
+          >
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-2 border border-gray-400 rounded-xl"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <input 
+              type="email" 
+              placeholder="Email" 
+              className="p-2 border border-gray-400 rounded-xl"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="p-2 border border-gray-400 rounded-xl"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input 
+              type="password" 
+              placeholder="Confirm Password" 
+              className="p-2 border border-gray-400 rounded-xl"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button 
+              type="submit" 
+              className="bg-green-500 text-white p-2 rounded-xl hover:scale-101"
+            >
+              Register
+            </button>
+          </form>
+          <Link to="/login" className=" hover:underline mt-4 inline-block">
+            <p>Already have an account</p>
+          </Link>
+          
+        </div>
+      </div>
     </div>
   )
 }
